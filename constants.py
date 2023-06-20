ab2228e0b8d4c570c268d3942a10a1b4e878e088
Wesley-Breno <jaosdc456@outlook.com>
2023-06-20 15:23:16 -0300

    melhorando estrutura dos arquivos

---
+++ constants.py
@@ -0,0 +1,40 @@
+LINKS_CAMPEONATOS_GP1 = [
+    'https://www.gp1.com.br/tabelas/campeonato-brasileiro-a/',
+    'https://www.gp1.com.br/tabelas/campeonato-brasileiro-b/',
+    'https://www.gp1.com.br/tabelas/campeonato-brasileiro-c/',
+    'https://www.gp1.com.br/tabelas/campeonato-brasileiro-d/',
+    'https://www.gp1.com.br/tabelas/campeonato-alagoano/',
+    'https://www.gp1.com.br/tabelas/campeonato-baiano/',
+    'https://www.gp1.com.br/tabelas/campeonato-capixaba/',
+    'https://www.gp1.com.br/tabelas/campeonato-carioca/',
+    'https://www.gp1.com.br/tabelas/campeonato-catarinense/',
+    'https://www.gp1.com.br/tabelas/campeonato-cearense/',
+    'https://www.gp1.com.br/tabelas/campeonato-gaucho/',
+    'https://www.gp1.com.br/tabelas/campeonato-goiano/',
+    'https://www.gp1.com.br/tabelas/campeonato-maranhense/',
+    'https://www.gp1.com.br/tabelas/campeonato-matogrossense/',
+    'https://www.gp1.com.br/tabelas/campeonato-mineiro/',
+    'https://www.gp1.com.br/tabelas/campeonato-paraense/',
+    'https://www.gp1.com.br/tabelas/campeonato-paraibano/',
+    'https://www.gp1.com.br/tabelas/campeonato-paranaense/',
+    'https://www.gp1.com.br/tabelas/campeonato-paulista/',
+    'https://www.gp1.com.br/tabelas/campeonato-pernambucano/',
+    'https://www.gp1.com.br/tabelas/campeonato-piauiense/',
+    'https://www.gp1.com.br/tabelas/campeonato-potiguar/',
+    'https://www.gp1.com.br/tabelas/campeonato-sergipano/',
+    'https://www.gp1.com.br/tabelas/campeonato-sul-matogrossense/',
+    'https://www.gp1.com.br/tabelas/campeonato-sul-matogrossense/',
+    'https://www.gp1.com.br/tabelas/copa-do-mundo-fifa/',
+    'https://www.gp1.com.br/tabelas/copa-sul-americana/',
+    'https://www.gp1.com.br/tabelas/eliminatorias-da-copa-do-mundo/',
+    'https://www.gp1.com.br/tabelas/copa-libertadores-da-america/',
+    'https://www.gp1.com.br/tabelas/liga-dos-campeoes-da-uefa/'
+]
+
+ANALISES_FEITAS = dict()
+
+TIMES_PONTUACOES = dict()
+
+JOGOS_PENDENTES = []
+
+NOMES_CAMPEONATOS = []
